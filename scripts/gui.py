7879385c3a70c4ef55f1f29200e43e839ae56556
Jothin kumar <bjothinphysics@gmail.com>
2021-11-15 12:01:08 +0530

    Created several levels

--- scripts/gui.py
+++ scripts/gui.py
@@ -24,7 +24,7 @@ def main():
     def create_rectangle_in_random_spot():
         previous_rectangle = None
         try:
-            while True:
+            for _ in range(20):
                 if previous_rectangle is not None:
                     play_area.delete(previous_rectangle)
                 base = randint(0, 850)
@@ -33,6 +33,22 @@ def main():
                 play_area.tag_bind(rectangle, '<Button-1>', increase_score_command)
                 previous_rectangle = rectangle
                 sleep(1.5)
+            for _ in range(30):
+                play_area.delete(previous_rectangle)
+                base = randint(0, 850)
+                rectangle = play_area.create_rectangle(base, base + 50, base + 100, base + 150, fill='red',
+                                                       outline='red')
+                play_area.tag_bind(rectangle, '<Button-1>', increase_score_command)
+                previous_rectangle = rectangle
+                sleep(1)
+            while True:
+                play_area.delete(previous_rectangle)
+                base = randint(0, 850)
+                rectangle = play_area.create_rectangle(base, base + 50, base + 100, base + 150, fill='red',
+                                                       outline='red')
+                play_area.tag_bind(rectangle, '<Button-1>', increase_score_command)
+                previous_rectangle = rectangle
+                sleep(0.5)
         except RuntimeError:
             pass
 
